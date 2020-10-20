a2d04991b3837a7a80ceb25f59513a8974ab2e22
Ichicoro <me@marte.dev>
2020-10-20 16:42:16 +0200

    Add death barrier and stuff :D

---
+++ Assets/DeathBarrier.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class DeathBarrier : NetworkBehaviour {
+    public Transform spawnPoint;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        other.transform.position = spawnPoint.position;
+        other.attachedRigidbody.velocity = new Vector3(0, 0, 0);
+    }
+
+}
